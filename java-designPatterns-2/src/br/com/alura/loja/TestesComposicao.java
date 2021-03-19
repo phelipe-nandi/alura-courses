8a33fac564d35093aa7f49283c3b71ff9008702d
phelipe-nandi <phelipe.nandi@sc.senai.br>
2021-03-19 11:55:23 -0300

    Finalizado o curso java-designPatterns-2

--- java-designPatterns-2/src/br/com/alura/loja/TestesComposicao.java
+++ java-designPatterns-2/src/br/com/alura/loja/TestesComposicao.java
@@ -2,6 +2,7 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.orcamento.OrcamentoProxy;
 
 import java.math.BigDecimal;
 
@@ -17,6 +18,13 @@ public class TestesComposicao {
         novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
         novo.adicionarItem(antigo);
 
-        System.out.println(novo.getValor());
+        OrcamentoProxy orcamentoProxy = new OrcamentoProxy(novo);
+
+        System.out.println(orcamentoProxy.getValor());
+        System.out.println(orcamentoProxy.getValor());
+        System.out.println(orcamentoProxy.getValor());
+        System.out.println(orcamentoProxy.getValor());
+        System.out.println(orcamentoProxy.getValor());
+        System.out.println(orcamentoProxy.getValor());
     }
 }
