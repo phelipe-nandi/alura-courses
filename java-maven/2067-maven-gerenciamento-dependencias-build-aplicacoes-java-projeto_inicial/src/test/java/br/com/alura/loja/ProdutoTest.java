2ae938fed4c7ee1423465a8c56a40615d62f9fdb
phelipe-nandi <phelipe.nandi@sc.senai.br>
2021-04-13 09:55:40 -0300

    Finalizado curso java-maven

--- java-maven/2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ java-maven/2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -2,13 +2,18 @@ package br.com.alura.loja;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Produto produto = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", produto.getNome());
+		Assert.assertEquals(BigDecimal.TEN, produto.getPreco());
 	}
 
 }
